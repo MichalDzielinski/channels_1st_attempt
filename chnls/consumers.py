597821438123a366e8077a655559cbbcbbc28490
Michał Dzieliński <dzielinski.michal@gmail.com>
2023-08-21 15:44:41 +0200

    Sub

--- chnls/consumers.py
+++ chnls/consumers.py
@@ -4,11 +4,19 @@ import json
 class DashboardConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        print('connect')
+        dashboard_slug = self.scope['url_route']['kwargs']['dashboard_slug']
+        self.dashboard_slug = dashboard_slug
+        self.room_group_name = f'chnls-{dashboard_slug}'
+        self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
     
     async def disconnect(self, close_code):
         print(f'connection closed {close_code}')
+
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
     
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -18,9 +26,26 @@ class DashboardConsumer(AsyncWebsocketConsumer):
         print(message)
         print(sender)
 
-        print(message, sender)
+        await self.channel_layer.group_send(
+            self.room_group_name, 
+            {
+                'type': 'statistics_message',
+                'message': message,
+                'sender': sender,
+            }
+        )
+
+        # await self.send(text_data=json.dumps({
+        #     'message': 'hellow from hier',
+        #     'sender': sender,
+        # }))
+
+    async def statistics_message(self, event):
+
+        message = event['message']
+        sender = event['sender']
 
         await self.send(text_data=json.dumps({
-            'message': 'hellow from hier',
-            # 'sender': sender,
+            'message': message,
+            'sender': sender,
         }))
--- chnls/static/chnls/dashboard.js
+++ chnls/static/chnls/dashboard.js
@@ -1,16 +1,30 @@
 console.log('Hello, this is Tony Hawk');
 
+const dashboardSlug = document.getElementById('dashboard-slug').textContent.trim()
+const user = document.getElementById('user').textContent.trim()
 
-const socket = new WebSocket('ws://'+window.location.host+'/ws/some-stati/')
+const submitBtn = document.getElementById('submit-btn')
+const dataInput = document.getElementById('data-input')
+const dataBox = document.getElementById('data-box')
+
+const socket = new WebSocket(`ws://${window.location.host}/ws/${dashboardSlug}/`)
 console.log(socket);
 
 socket.onmessage = function(e){
-    console.log('Server: '+ e.data);
+    console.log('server' );
+    const {sender,  message} = JSON.parse(e.data)
+    console.log(message) 
+    console.log(sender) 
+    dataBox.innerHTML += `<p>${sender}: ${message}</p>`
+   
 };
 
-socket.onopen = function(e){
+submitBtn.addEventListener('click', () => {
+    const dataValue = dataInput.value 
     socket.send(JSON.stringify({
-        'message': "Hello from client",
-        'sender': "test sender",
+        'message': dataValue,
+        'sender': user,
     }))
-}
+
+})
+
